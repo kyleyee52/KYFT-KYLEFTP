01196ca728f50130bd3b9249058138eb12b04441
Leaseback <kmk4427@rit.edu>
2023-01-10 19:42:49 -0500

    Client.py changes

--- client.py
+++ client.py
@@ -1,19 +1,26 @@
 from ftplib import FTP, all_errors
 from connect import connect, disconnect
 
-# Get FTP Server host address and port from user
-host = input("Enter FTP server host address: ")
+# Handles log in and initiates a command loop
+def main():
+    # Get FTP Server host address and port from user
+    host = input("Enter FTP server host address: ")
 
-# Get login credentials from the user
-username = input("Enter Username: ")
-password = input("Enter password: ")
+    # Get login credentials from the user
+    username = input("Enter Username: ")
+    password = input("Enter password: ")
 
-# Connect to FTP Server using given host, username, and password
-connect(host, username, password)
+    # Connect to FTP Server using given host, username, and password
+    connect(host, username, password)
 
-# TODO MAIN LOOP
+    # Command loop:
+    command = input("Enter command. Type help for list of all commands")
+    # TODO MAIN LOOP
 
-# Disconnect from the FTP Server after main loop
-disconnect()
+    # Disconnect from the FTP Server after main loop
+    disconnect()
 
 exit("Program ran successfully.")
+
+if __name__ == "__main__":
+    main()
